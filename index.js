2edd7049dcafefc449f929118661bdec69082684
Daniel Gary <danielgary@live.com>
2017-01-24 14:58:23 -0600

    fix for index

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ function generateServerFromContainer(container) {
     result = result.appendLine(`\t\tlocation / {`)
       .appendLine(`\t\t\tadd_header Set-Cookie "HUB_URL=${container.environment.HUB_URL};Domain=${container.web.server_names[0]};Path=/;Max-Age=31536000";`)
       .appendLine(`\t\t\tadd_header Set-Cookie "PACKAGE_ID=${container.environment.PACKAGE_ID};Domain=${container.web.server_names[0]};Path=/;Max-Age=31536000";`)
-      .appendLine(`\t\t\ttry_files $uri @api;`)
+      .appendLine(`\t\t\ttry_files $uri $uri/index.html @api;`)
       .appendLine(`\t\t\texpires max;`)
       .appendLine(`\t\t}`)
 
